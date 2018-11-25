a3c78801db5c480ee19351beef4c541cad6120ef
Cj <ckozeny@unomaha.edu>
2018-11-24 23:19:02 -0600

    Try one

---
+++ project_sample/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Event
+from .models import Event
+
+
+class EventList(admin.ModelAdmin):
+    list_display = ('event_name', 'organization', 'contact_number' )
+    list_filter = 'organization'
+    search_fields = 'organization'
+    ordering = ['event_name']
+
+admin.site.register(Event)
+
